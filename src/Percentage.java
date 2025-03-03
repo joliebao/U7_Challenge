c703b42f200b57e98db84aa15035a2255b4ff88c
BT_3N3_31 <baojolie@gmail>
2025-03-03 10:53:03 -0500

    Percentage is still too low

--- src/Percentage.java
+++ src/Percentage.java
@@ -7,18 +7,12 @@ public class Percentage {
         original = og;
         newOne = n;
     }
-
+    // l, w, x, y
     public void overLap() {
-        if (original[2] == newOne[2] && original[3] == newOne[3]) { // if x and ys are the same
-            counter++;
-        } else if (original[2] == newOne[2] && original[3] < newOne[3] && newOne[3] < (original[3] + original[0])) { // if x is the same and y is in range
-            counter++;
-        } else if (original[3] == newOne[3] && original[2] < newOne[2] && newOne[2] < (original[2] + original[1])) { // if y is the same and x is in range
-            counter++;
-        } else if (original[2] < newOne[2] && newOne[2] < (original[2] + original[1]) && original[3] < newOne[3] && newOne[3] < (original[3] + original[0])) { // if x and y are in range
-            counter++;
-        } else if (newOne[2] < original[2] && original[2] < (newOne[2] + newOne[1]) && newOne[3] < original[3] && original[3] < (newOne[3] + newOne[0])) { // if x and y are in range
-            counter++;
+        if (original[2] <= newOne[2] && newOne[2] <= (original[2] + original[1])){
+            if (original[3] <= newOne[3] && newOne[3] <= (original[3] + original[0])) { // if x and y are in range
+                counter++;
+            }
         }
     }
 
