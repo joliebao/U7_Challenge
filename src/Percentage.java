3e9edc91d3640d5703d5cd8dd3010ee1630d0519
BT_3N3_31 <baojolie@gmail>
2025-03-03 10:59:08 -0500

    Percentage is too high

--- src/Percentage.java
+++ src/Percentage.java
@@ -9,10 +9,12 @@ public class Percentage {
     }
     // l, w, x, y
     public void overLap() {
-        if (original[2] <= newOne[2] && newOne[2] <= (original[2] + original[1])){
-            if (original[3] <= newOne[3] && newOne[3] <= (original[3] + original[0])) { // if x and y are in range
-                counter++;
-            }
+        if (original[2] <= newOne[2] && newOne[2] <= (original[2] + original[1])){ // if x is in range
+            // need a y condition
+            counter++;
+        } else if (original[3] <= newOne[3] && newOne[3] <= (original[3] + original[0])) { // if y is in range
+            // need an x condition
+            counter++;
         }
     }
 
